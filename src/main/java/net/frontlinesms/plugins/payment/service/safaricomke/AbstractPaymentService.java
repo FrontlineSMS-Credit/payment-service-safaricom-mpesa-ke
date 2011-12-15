e2e8495b56b4c717d46db79164a4eaf38d2fd82d
Alex Anderson <alex@frontlinesms.com>
2011-12-15 10:01:13 +0300

    Cleaned up code in AbstractPaymentService.

--- src/main/java/net/frontlinesms/plugins/payment/service/safaricomke/AbstractPaymentService.java
+++ src/main/java/net/frontlinesms/plugins/payment/service/safaricomke/AbstractPaymentService.java
@@ -38,7 +38,7 @@ import org.smslib.SMSLibDeviceException;
 import org.smslib.handler.ATHandler.SynchronizedWorkflow;
 
 public abstract class AbstractPaymentService implements PaymentService, EventObserver {
-//> PERSISTENT PROPERTIES
+//> STATIC CONSTANTS
 	/** Prefix attached to every property name. */
 	private static final String PROPERTY_PREFIX = "plugins.payment.mpesa.";
 
@@ -49,6 +49,67 @@ public abstract class AbstractPaymentService implements PaymentService, EventObs
 	protected static final String PROPERTY_BALANCE_UPDATE_METHOD = PROPERTY_PREFIX + "balance.update.method";
 	protected static final String PROPERTY_MODEM_SERIAL = PROPERTY_PREFIX + "modem.serial";
 	
+//> INSTANCE PROPERTIES
+	protected Logger log = FrontlineUtils.getLogger(this.getClass());
+	protected TargetAnalytics targetAnalytics;
+	protected CService cService;
+	protected EventBus eventBus;
+	protected PaymentJobProcessor requestJobProcessor;
+	protected PaymentJobProcessor responseJobProcessor;
+	protected AccountDao accountDao;
+	protected ClientDao clientDao;
+	protected TargetDao targetDao;
+	protected IncomingPaymentDao incomingPaymentDao;
+	protected OutgoingPaymentDao outgoingPaymentDao;
+	protected LogMessageDao logDao;
+	protected ContactDao contactDao;
+	private PersistableSettings settings;
+	
+//> CONSTRUCTORS AND INITIALISERS
+	// FIXME this is not currently called
+	public void initDaosAndServices(final PaymentViewPluginController pluginController) {
+		this.accountDao = pluginController.getAccountDao();
+		this.clientDao = pluginController.getClientDao();
+		this.outgoingPaymentDao = pluginController.getOutgoingPaymentDao();
+		this.targetDao = pluginController.getTargetDao();
+		this.incomingPaymentDao = pluginController.getIncomingPaymentDao();
+		this.targetAnalytics = pluginController.getTargetAnalytics();
+		this.logDao = pluginController.getLogMessageDao();
+		this.contactDao = pluginController.getUiGeneratorController().getFrontlineController().getContactDao();
+		
+		this.registerToEventBus(
+			pluginController.getUiGeneratorController().getFrontlineController().getEventBus()
+		);
+		
+		this.requestJobProcessor = new PaymentJobProcessor(this);
+		this.requestJobProcessor.start();
+		
+		this.responseJobProcessor = new PaymentJobProcessor(this);
+		this.responseJobProcessor.start();
+	}
+
+	protected void initIfRequired() throws SMSLibDeviceException, IOException {
+		// For now, we assume that init is always required.  If there is a clean way
+		// of identifying when it is and is not, we should perhaps implement this.
+		this.cService.getAtHandler().stkInit();
+	}
+	
+//> INSTANCE (TRANSIENT) ACCESSORS
+	public CService getCService() {
+		return cService;
+	}
+	public void setCService(final CService cService) {
+		this.cService = cService;
+	}
+	/** @return the settings attached to this instance. */
+	public PersistableSettings getSettings() {
+		return settings;
+	}
+	public void setSettings(PersistableSettings settings) {
+		this.settings = settings;
+	}
+	
+//> PERSISTENT PROPERTY ACCESSORS
 	public String getBalanceConfirmationCode() {
 		return getProperty(PROPERTY_BALANCE_CONFIRMATION_CODE, String.class);
 	}
@@ -76,29 +137,20 @@ public abstract class AbstractPaymentService implements PaymentService, EventObs
 	public String getPsSmsModemSerial() {
 		return getProperty(PROPERTY_MODEM_SERIAL, String.class);
 	}
+	public String getPin() {
+		return getProperty(PROPERTY_PIN, String.class);
+	}
+	public void setPin(final String pin) {
+		setProperty(PROPERTY_PIN, pin);
+	}
 	void updateBalance(BigDecimal amount, String confirmationCode, Date timestamp, String method) {
 		setBalanceAmount(amount);
 		setBalanceConfirmationCode(confirmationCode);
 		setBalanceDateTime(timestamp);
 		setBalanceUpdateMethod(method);
 	}
-	
-	protected CService cService;
-	protected EventBus eventBus;
-	protected PaymentJobProcessor requestJobProcessor;
-	protected PaymentJobProcessor responseJobProcessor;
-	protected AccountDao accountDao;
-	protected ClientDao clientDao;
-	protected TargetDao targetDao;
-	protected IncomingPaymentDao incomingPaymentDao;
-	protected OutgoingPaymentDao outgoingPaymentDao;
-	protected LogMessageDao logDao;
-	protected ContactDao contactDao;
-	private PersistableSettings settings;
-	
-	protected Logger log = FrontlineUtils.getLogger(this.getClass());
-	protected TargetAnalytics targetAnalytics;
 
+//> CONFIGURABLE SERVICE METHODS
 	public Class<? extends ConfigurableService> getSuperType() {
 		return PaymentService.class;
 	}
@@ -125,137 +177,10 @@ public abstract class AbstractPaymentService implements PaymentService, EventObs
 		});
 	}
 
-	@SuppressWarnings("rawtypes")
-	public void notify(final FrontlineEventNotification notification) {
-		if(notification instanceof EntitySavedNotification) {
-			final Object entity = ((EntitySavedNotification) notification).getDatabaseEntity();
-			if (entity instanceof FrontlineMessage) {
-				final FrontlineMessage message = (FrontlineMessage) entity;
-				processMessage(message);
-			}
-		}
-	}
-	
-	protected abstract void processMessage(final FrontlineMessage message);
-
-	protected String getFirstMatch(final String string, final String regexMatcher) {
-		final Matcher matcher = Pattern.compile(regexMatcher).matcher(string);
-		matcher.find();
-		return matcher.group();
-	}
-
-	protected String getFirstMatch(final FrontlineMessage message, final String regexMatcher) {
-		return getFirstMatch(message.getTextContent(), regexMatcher);
-	}
-
-	@Override
-	public boolean equals(final Object other) {
-		if (!(other instanceof PaymentService)){
-			return false;
-		}
-		
-		if (!(other instanceof MpesaPaymentService)){
-			return false;
-		}
-		
-		return super.equals(other);
-	}
-
-	protected void initIfRequired() throws SMSLibDeviceException,
-			IOException {
-				// For now, we assume that init is always required.  If there is a clean way
-				// of identifying when it is and is not, we should perhaps implement this.
-				this.cService.getAtHandler().stkInit();
-			}
-
 	public void stopService() {
 		eventBus.unregisterObserver(this);
 		requestJobProcessor.stop();
 		responseJobProcessor.stop();
-	}
-	
-	protected <T extends Object> T getProperty(String key, Class<T> clazz) {
-		return PersistableSettings.getPropertyValue(getPropertiesStructure(), settings, key, clazz);
-	}
-	
-	/**
-	 * Sets a property in {@link #settings}.
-	 * @param key
-	 * @param value
-	 */
-	protected void setProperty(String key, Object value) {
-		this.settings.set(key, value);
-	}
-
-	public String getPin() {
-		return getProperty(PROPERTY_PIN, String.class);
-	}
-
-	public void registerToEventBus(final EventBus eventBus) {
-		if (eventBus != null) {
-			this.eventBus = eventBus;
-			this.eventBus.registerObserver(this);
-		}
-	}
-
-	/** @return the settings attached to this instance. */
-	public PersistableSettings getSettings() {
-		return settings;
-	}
-
-	public void setPin(final String pin) {
-		setProperty(PROPERTY_PIN, pin);
-	}
-
-	public void setCService(final CService cService) {
-		this.cService = cService;
-	}
-	
-//	public void setBalanceDispatcher(BalanceDispatcher balanceDispatcher) {
-//		this.balanceDispatcher = balanceDispatcher;
-//	}
-	
-	public void initDaosAndServices(final PaymentViewPluginController pluginController) {
-		this.accountDao = pluginController.getAccountDao();
-		this.clientDao = pluginController.getClientDao();
-		this.outgoingPaymentDao = pluginController.getOutgoingPaymentDao();
-		this.targetDao = pluginController.getTargetDao();
-		this.incomingPaymentDao = pluginController.getIncomingPaymentDao();
-		this.targetAnalytics = pluginController.getTargetAnalytics();
-		this.logDao = pluginController.getLogMessageDao();
-		this.contactDao = pluginController.getUiGeneratorController().getFrontlineController().getContactDao();
-		
-		this.registerToEventBus(
-			pluginController.getUiGeneratorController().getFrontlineController().getEventBus()
-		);
-		
-		this.requestJobProcessor = new PaymentJobProcessor(this);
-		this.requestJobProcessor.start();
-		
-		this.responseJobProcessor = new PaymentJobProcessor(this);
-		this.responseJobProcessor.start();
-	}
-	
-	void queueRequestJob(PaymentJob job) {
-		requestJobProcessor.queue(job);
-	}
-	
-	void queueResponseJob(PaymentJob job) {
-		responseJobProcessor.queue(job);
-	}
-
-	public CService getCService() {
-		return cService;
-	}
-	
-	public void updateStatus(PaymentStatus sending) {
-		if (eventBus != null){
-			eventBus.notifyObservers(new PaymentStatusEventNotification(sending));
-		}
-	}
-	
-	public void setSettings(PersistableSettings settings) {
-		this.settings = settings;
 	}
 
 	public StructuredProperties getPropertiesStructure() {
@@ -268,10 +193,63 @@ public abstract class AbstractPaymentService implements PaymentService, EventObs
 		p.put(PROPERTY_MODEM_SERIAL, "");
 		return p;
 	}
+
+//> EVENT OBSERVER METHODS
+	@SuppressWarnings("rawtypes")
+	public void notify(final FrontlineEventNotification notification) {
+		if(notification instanceof EntitySavedNotification) {
+			final Object entity = ((EntitySavedNotification) notification).getDatabaseEntity();
+			if (entity instanceof FrontlineMessage) {
+				final FrontlineMessage message = (FrontlineMessage) entity;
+				processMessage(message);
+			}
+		}
+	}
 	
+//> ABSTRACT SAFARICOM SERVICE METHODS
+	protected abstract void processMessage(final FrontlineMessage message);
 	abstract Date getTimePaid(FrontlineMessage message);
 	abstract boolean isMessageTextValid(String message);
 	abstract Account getAccount(FrontlineMessage message);
 	abstract String getPaymentBy(FrontlineMessage message);
 	protected abstract boolean isValidBalanceMessage(FrontlineMessage message);
+
+//> UTILITY METHODS
+	void queueRequestJob(PaymentJob job) {
+		requestJobProcessor.queue(job);
+	}
+	void queueResponseJob(PaymentJob job) {
+		responseJobProcessor.queue(job);
+	}
+	
+	/** Gets a property from {@link #settings}. */
+	<T extends Object> T getProperty(String key, Class<T> clazz) {
+		return PersistableSettings.getPropertyValue(getPropertiesStructure(), settings, key, clazz);
+	}
+	/** Sets a property in {@link #settings}. */
+	void setProperty(String key, Object value) {
+		this.settings.set(key, value);
+	}
+
+	protected String getFirstMatch(final FrontlineMessage message, final String regexMatcher) {
+		return getFirstMatch(message.getTextContent(), regexMatcher);
+	}	
+	protected String getFirstMatch(final String string, final String regexMatcher) {
+		final Matcher matcher = Pattern.compile(regexMatcher).matcher(string);
+		matcher.find();
+		return matcher.group();
+	}
+
+	void registerToEventBus(final EventBus eventBus) {
+		if (eventBus != null) {
+			this.eventBus = eventBus;
+			this.eventBus.registerObserver(this);
+		}
+	}
+	
+	void updateStatus(PaymentStatus sending) {
+		if (eventBus != null){
+			eventBus.notifyObservers(new PaymentStatusEventNotification(sending));
+		}
+	}
 }
